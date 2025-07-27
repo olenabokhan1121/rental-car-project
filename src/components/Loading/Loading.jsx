60106ffd329af80e35bcea83d5d94504ca1dcbbc
Lena_Bokhan <leliklena@i.ua>
2025-07-27 23:47:15 +0300

    change loader

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,25 +1,8 @@
 import css from "../Loading/Loading.module.css";
-
-import {
-    ThreeDots
-}
-
-from "react-loader-spinner";
-
 export default function Loading() {
-    return (<div className= {
-            css.loader
-        }
-
-        > <ThreeDots height="80"
-        width="80"
-        radius="9"
-        color="#3a5a40"
-        ariaLabel="three-dots-loading"
-
-        visible= {
-            true
-        }
-
-        /> </div>);
+  return (
+    <div className={css.loader}>
+      <div className={css.spinner}></div>
+    </div>
+  );
 }
